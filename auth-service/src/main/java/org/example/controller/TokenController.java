f6cd1d91ee06378ea2a5c3a2b319fe3340c8057c
dcharilasAW <dimitris.charilas@allwyn-lotterysolutions.com>
2024-02-09 10:29:23 +0200

    Adds functionality to retrieve token

--- auth-service/src/main/java/org/example/controller/TokenController.java
+++ auth-service/src/main/java/org/example/controller/TokenController.java
@@ -1,15 +1,21 @@
 package org.example.controller;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import org.example.model.integration.TokenResponse;
 import org.example.service.ProviderIntegrationService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-@RestController("/token")
+@RestController
 public class TokenController {
+
+    private final static String AUTH_COOKIE_NAME = "auth-cookie";
 
     private ProviderIntegrationService service;
 
@@ -17,10 +23,13 @@ public class TokenController {
         this.service = service;
     }
 
-    @PostMapping(path = "/", produces = APPLICATION_JSON_VALUE)
-    public TokenResponse getToken(HttpServletRequest request) {
-        //TODO read info from cookie
-        request.getCookies();
-        return service.getAccessToken("-","-");
+    @PostMapping(path = "/token", produces = APPLICATION_JSON_VALUE)
+    public TokenResponse getToken(HttpServletRequest request, @RequestParam String clientName) {
+        return service.getAccessToken(clientName,getCodeFromCookie(request.getCookies()));
+    }
+
+    private String getCodeFromCookie(Cookie[] cookies) {
+        //TODO throw exception if cookie is not found
+        return Arrays.asList(cookies).stream().filter(c -> AUTH_COOKIE_NAME.equals(c.getName())).findFirst().get().getValue();
     }
 }
--- auth-service/src/main/java/org/example/service/ProviderIntegrationService.java
+++ auth-service/src/main/java/org/example/service/ProviderIntegrationService.java
@@ -1,13 +1,16 @@
 package org.example.service;
 
 import org.example.conf.ClientsProperties;
-import org.example.model.integration.TokenRequest;
 import org.example.model.integration.TokenResponse;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
 
 import static java.util.Objects.nonNull;
 
@@ -22,16 +25,17 @@ public class ProviderIntegrationService {
 
     public TokenResponse getAccessToken(String clientName, String code) {
         ClientsProperties.Client client = clientsProperties.getClient(clientName);
-        TokenRequest requestBody = TokenRequest.builder()
-                .redirectUri(client.getRedirectUri())
-                .clientId(client.getClientId())
-                .clientSecret(client.getClientSecret())
-                .code(code)
-                .build();
-
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<String> request = new HttpEntity<>(requestBody.toString(), headers);
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String,String> requestBody = new LinkedMultiValueMap();
+        requestBody.put("grant_type", Collections.singletonList("authorization_code"));
+        requestBody.put("code", Collections.singletonList(code));
+        requestBody.put("redirect_uri", Collections.singletonList(client.getRedirectUri()));
+        requestBody.put("client_id", Collections.singletonList(client.getClientId()));
+        requestBody.put("client_secret", Collections.singletonList(client.getClientSecret()));
+
+        HttpEntity<MultiValueMap<String,String>> request = new HttpEntity<>(requestBody, headers);
 
         RestTemplate restTemplate = new RestTemplate();
         TokenResponse result = restTemplate.postForObject(constructUrlWithParams(client), request, TokenResponse.class);
